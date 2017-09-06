8d9814c01107c51a0b63541e1c535b697c7cbb5a
dongxiaopeng <dongxiaopeng@zuozh.com>
2017-09-06 15:07:29 +0800

    重入锁

---
+++ src/jdk_concurrent/ReenterLock.java
@@ -0,0 +1,44 @@
+package jdk_concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 重入锁
+ * 
+ * @author cclouds
+ *
+ */
+public class ReenterLock implements Runnable {
+
+	public static ReentrantLock lock = new ReentrantLock();
+
+	public static int i = 0;
+
+	@Override
+	public void run() {
+		for (int j = 0; j < 10000000; j++) {
+			lock.lock();
+			try {
+				i++;
+			} catch (Exception e) {
+				// TODO: handle exception
+			} finally {
+				lock.unlock();
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		ReenterLock tl = new ReenterLock();
+		Thread t1 = new Thread(tl);
+		Thread t2 = new Thread(tl);
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println(i);
+	}
+
+}
