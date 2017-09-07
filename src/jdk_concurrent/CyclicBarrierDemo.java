cf7f90495eaa7e91feee2bfc7abd01fe3c07c229
dongxiaopeng <dongxiaopeng@zuozh.com>
2017-09-07 16:03:02 +0800

    循环栅栏

---
+++ src/jdk_concurrent/CyclicBarrierDemo.java
@@ -0,0 +1,92 @@
+package jdk_concurrent;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * 循环栅栏（CyclicBarrier）
+ * 
+ * @author cclouds
+ *
+ */
+public class CyclicBarrierDemo {
+	public static class Soldier implements Runnable {
+
+		private String soldier;
+		private final CyclicBarrier cyclic;
+
+		public Soldier(CyclicBarrier cyclic,String soldier) {
+			super();
+			this.soldier = soldier;
+			this.cyclic = cyclic;
+		}
+
+		@Override
+		public void run() {
+			try {
+				// 等待所有士兵到齐
+				cyclic.await();
+				doWork();
+				// 等待所有士兵完成任务
+				cyclic.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (BrokenBarrierException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		void doWork() {
+			try {
+				Thread.sleep(new Random().nextInt(10) * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(soldier + ":任务完成");
+		}
+
+	}
+
+	public static class BarrierRun implements Runnable {
+
+		boolean flag;
+		int N;
+
+		public BarrierRun(boolean flag, int n) {
+			super();
+			this.flag = flag;
+			N = n;
+		}
+
+		@Override
+		public void run() {
+			if (flag) {
+				System.out.println("司令：士兵" + N + "个，完成任务！");
+			} else {
+				System.out.println("司令：士兵" + N + "个，集合完毕！");
+				flag = true;
+			}
+		}
+
+	}
+	
+	
+	public static void main(String[] args) {
+		final int N =10;
+		boolean flag = false;
+		Thread[] allSoldier = new Thread[N];
+		
+		CyclicBarrier cyclic = new CyclicBarrier(N, new BarrierRun(flag, N));
+		//设置屏障点，主要为了执行这个方法
+		System.out.println("集合队伍！");
+		for(int i=0;i<N;i++){
+			System.out.println("士兵"+i+"报道！");
+			allSoldier[i] = new Thread(new Soldier(cyclic, "士兵"+i));
+			allSoldier[i].start();
+		}
+		
+	}
+
+}
