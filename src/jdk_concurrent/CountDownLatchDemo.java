8efa6e737ccf3e573e0f738b1f125049c04e377a
dongxiaopeng <dongxiaopeng@zuozh.com>
2017-09-07 11:16:49 +0800

    倒计时器

---
+++ src/jdk_concurrent/CountDownLatchDemo.java
@@ -0,0 +1,41 @@
+package jdk_concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+/**
+ * 倒计时器（CountDownLatch）
+ * @author cclouds
+ *
+ */
+public class CountDownLatchDemo implements Runnable{
+
+	static final CountDownLatch latch = new CountDownLatch(10);
+	static final CountDownLatchDemo demo = new CountDownLatchDemo();
+	@Override
+	public void run() {
+  
+		try {
+			Thread.sleep(1000);
+			System.out.println("check complete");
+			latch.countDown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService exec = Executors.newFixedThreadPool(10);
+		
+		for(int i=0;i<10;i++){
+			exec.submit(demo);
+		}
+		
+		latch.await();
+		//发射
+		System.out.println("Fire!");
+		
+		exec.shutdown();
+	}
+
+}
